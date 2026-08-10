844389b0c8dff4dab383504caa4a04bbfe7b84be
Asrın Deniz Ak <asrinak1905@gmail.com>
2026-08-11 00:34:35 +0300

    seat authorization

--- src/main/java/com/flightapp/flightticketsystem/controller/SeatController.java
+++ src/main/java/com/flightapp/flightticketsystem/controller/SeatController.java
@@ -6,6 +6,7 @@ import com.flightapp.flightticketsystem.service.ISeatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class SeatController {
 
     private final ISeatService seatService;
 
+    @PreAuthorize("hasRole('ADMIN')") //Bu metoda sadece yetkilerinde ROLE_ADMIN olan kişiler girebilir diyoruz
     @PostMapping("/add/seats")
     public ResponseEntity<SeatResponseDto> addSeat(@RequestBody SeatRequestDto requestDto) {
         SeatResponseDto response = seatService.addSeat(requestDto);
@@ -33,11 +35,13 @@ public class SeatController {
         return ResponseEntity.ok(seatService.getAvailableSeatsByFlightId(flightId));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/seats/{id}")
     public ResponseEntity<SeatResponseDto> updateSeat(@PathVariable Integer id, @RequestBody SeatRequestDto requestDto) {
         return ResponseEntity.ok(seatService.updateSeat(id, requestDto));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteSeat(@PathVariable Integer id) {
         seatService.deleteSeat(id);
